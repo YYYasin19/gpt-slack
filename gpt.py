1e2a4bb4019d71f3b88a5dbe852a2bdc1802b593
Yasin Tatar <ytatar9@googlemail.com>
2022-12-03 16:07:35 +0100

    add checking the login

--- gpt.py
+++ gpt.py
@@ -5,30 +5,47 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 BROWSER_SETTINGS = webdriver.EdgeOptions()
-BROWSER_SETTINGS.add_argument("user-data-dir=selenium")
+BROWSER_SETTINGS.add_argument("user-data-dir=selenium3")
 
 class GPT:
 
     def __init__(self) -> None:
         self.browser = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=BROWSER_SETTINGS)
         self.browser.get("https://chat.openai.com")
-
-    def _load_gpt_page(self):
         self._check_login()
-
-    def _check_login(self) -> bool:
-        return True
 
     def _get_input_element(self):
         self._check_login()
         # return self.page.query_selector("div[class*='PromptTextarea__TextareaWrapper']").query_selector("textarea")
         # return self.page.wait_for_selector("div[class*='PromptTextarea__TextareaWrapper").wait_for_selector("textarea").bounding_box()
         return self.browser.find_element(By.CSS_SELECTOR, "div[class*='PromptTextarea__TextareaWrapper']").find_element(By.CSS_SELECTOR, "textarea")
+    
+    def _check_login(self):
+        if 'login' in self.browser.current_url or 'Log in with your OpenAI account to continue' in self.browser.page_source:
+            user, passw = os.environ['USERNAME'], os.environ['PASSWORD']
+            login_btn = self.browser.find_element(By.XPATH, '//*[@id="__next"]/div/div/div[4]/button[1]')
+            login_btn.click()
+
+            user_field = self.browser.find_element(By.XPATH, '//*[@id="username"]')
+            user_field.send_keys(user)
+            user_field.send_keys(Keys.ENTER)
+
+            passw_field = self.browser.find_element(By.XPATH, '//*[@id="password"]')
+            passw_field.send_keys(passw)
+            passw_field.send_keys(Keys.ENTER)
+            time.sleep(10) # safety, wait 10 seconds
+
+            while (nxt := self.browser.find_element(By.XPATH, "//*[text()='Next' or text()='Done']")):
+                nxt.click()
+                time.sleep(1)
+
     
     def query(self, query_str:str)->str:
         
